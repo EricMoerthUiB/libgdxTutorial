49f92b3c927e19c9c407c303b2b2649683b53bff
Eric Mörth <eric.moerth@uib.no>
2020-01-22 12:10:12 +0100

    Added player layer and move player on tiled map

--- src/main/java/inf112/skeleton/app/Tiled.java
+++ src/main/java/inf112/skeleton/app/Tiled.java
@@ -1,28 +1,27 @@
 package inf112.skeleton.app;
 
 import com.badlogic.gdx.*;
-import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.GL30;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
-import com.badlogic.gdx.graphics.g2d.Sprite;
-import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.maps.tiled.TiledMap;
 import com.badlogic.gdx.maps.tiled.TiledMapTileLayer;
+import com.badlogic.gdx.maps.tiled.TiledMapTileLayer.Cell;
 import com.badlogic.gdx.maps.tiled.TmxMapLoader;
 import com.badlogic.gdx.maps.tiled.renderers.OrthogonalTiledMapRenderer;
+import com.badlogic.gdx.maps.tiled.tiles.StaticTiledMapTile;
 import com.badlogic.gdx.math.Vector2;
 
 /**
  * RoboRally sprites by http://www.yeoldewebsiteknight.co.uk/roborally
  */
-public class Tiled extends InputAdapter implements ApplicationListener{
-
+public class Tiled extends InputAdapter implements ApplicationListener {
     // player properties
-    private Sprite sprite;
-    private SpriteBatch spriteBatch;
-    private Texture playerTexture;
+    private Cell playerCell;
+    private Cell playerDiedCell;
+    private Cell playerWonCell;
     private Vector2 playerPosition = new Vector2();
 
     // map properties
@@ -32,10 +31,10 @@ public class Tiled extends InputAdapter implements ApplicationListener{
 
     // layer
     private TiledMapTileLayer boardLayer;
+    private TiledMapTileLayer playerLayer;
     private TiledMapTileLayer collidableLayer;
     private TiledMapTileLayer holeLayer;
     private TiledMapTileLayer flagLayer;
-
 
     // camera properties
     private OrthogonalTiledMapRenderer renderer;
@@ -46,23 +45,24 @@ public class Tiled extends InputAdapter implements ApplicationListener{
 
     @Override
     public void create() {
-
         // register input processor
         Gdx.input.setInputProcessor(this);
 
         // load the map including the layers
         map = new TmxMapLoader().load("assets/example.tmx");
         boardLayer = (TiledMapTileLayer) map.getLayers().get("Board");
+        playerLayer = (TiledMapTileLayer) map.getLayers().get("Player");
         collidableLayer = (TiledMapTileLayer) map.getLayers().get("Collidable");
         holeLayer = (TiledMapTileLayer) map.getLayers().get("Hole");
         flagLayer = (TiledMapTileLayer) map.getLayers().get("Flag");
 
-        // load player and position it in the lower left corner
-        playerTexture = new Texture("uib.png");
+        // load Player on the tiled map
+        Texture playerTexture = new Texture("player.png");
+        TextureRegion[][] split = TextureRegion.split(playerTexture, 300, 300);
+        playerCell = new Cell().setTile(new StaticTiledMapTile(split[0][0]));
+        playerDiedCell = new Cell().setTile(new StaticTiledMapTile(split[0][1]));
+        playerWonCell = new Cell().setTile(new StaticTiledMapTile(split[0][2]));
         playerPosition.set(0, 0);
-        spriteBatch = new SpriteBatch();
-        sprite = new Sprite(playerTexture);
-        sprite.setScale(1 / 3f);
 
         // create an orthographic camera, shows us 5x5 units of the world
         camera = new OrthographicCamera();
@@ -73,10 +73,6 @@ public class Tiled extends InputAdapter implements ApplicationListener{
         // set the unit scale to 1/300f (1 unit == 300 pixels)
         renderer = new OrthogonalTiledMapRenderer(map, 1 / 300f);
         renderer.setView(camera);
-
-        // define win-lose messages
-        infoFont = new BitmapFont();
-        infoFont.setColor(new Color(1, 1, 0, 1)); // RGBA
     }
 
     @Override
@@ -89,32 +85,18 @@ public class Tiled extends InputAdapter implements ApplicationListener{
         Gdx.gl.glClearColor(0.5f, 0.5f, 0.5f, 1);
         Gdx.gl.glClear(GL30.GL_COLOR_BUFFER_BIT);
 
-        // compute ratios
-        float xAspectRatio = Gdx.graphics.getWidth()/(float)MAP_SIZE_X;
-        float yAspectRatio = Gdx.graphics.getHeight()/(float)MAP_SIZE_Y;
-
         // render map
         renderer.render();
 
         // update current player position
-        sprite.setPosition((playerPosition.x-1)*xAspectRatio, (playerPosition.y-1)*yAspectRatio);
-
-        // render player sprite
-        spriteBatch.begin();
-        sprite.draw(spriteBatch);
-        spriteBatch.end();
+        playerLayer.setCell((int) playerPosition.x, (int) playerPosition.y, playerCell);
 
         // check what field the player is standing on
-        if(holeLayer.getCell((int)playerPosition.x, (int)playerPosition.y) != null){
-            spriteBatch.begin();
-            infoFont.draw(spriteBatch, "LOST!", (playerPosition.x + 0.3f)*xAspectRatio, (playerPosition.y + 0.3f)*yAspectRatio);
-            spriteBatch.end();
+        if (holeLayer.getCell((int) playerPosition.x, (int) playerPosition.y) != null) {
+            playerLayer.setCell((int) playerPosition.x, (int) playerPosition.y, playerDiedCell);
         }
-
-        if(flagLayer.getCell((int)playerPosition.x, (int)playerPosition.y) != null){
-            spriteBatch.begin();
-            infoFont.draw(spriteBatch, "WON!", (playerPosition.x + 0.3f)*xAspectRatio, (playerPosition.y + 0.3f)*yAspectRatio);
-            spriteBatch.end();
+        if (flagLayer.getCell((int) playerPosition.x, (int) playerPosition.y) != null) {
+            playerLayer.setCell((int) playerPosition.x, (int) playerPosition.y, playerWonCell);
         }
     }
 
@@ -132,28 +114,25 @@ public class Tiled extends InputAdapter implements ApplicationListener{
 
     @Override
     public boolean keyUp(int keycode) {
+        //first clear the player cell so in case of moving the player avoid to have the player still showing at the old position
+        playerLayer.setCell((int) playerPosition.x, (int) playerPosition.y, null);
 
-        if(keycode == Input.Keys.LEFT) {
-
-            if(playerPosition.x > 0)
+        if (keycode == Input.Keys.LEFT) {
+            if (playerPosition.x > 0)
                 playerPosition.x -= 1;
         }
-        if(keycode == Input.Keys.RIGHT) {
-
-            if(playerPosition.x < MAP_SIZE_X-1)
+        if (keycode == Input.Keys.RIGHT) {
+            if (playerPosition.x < MAP_SIZE_X - 1)
                 playerPosition.x += 1;
         }
-        if(keycode == Input.Keys.UP) {
-
-            if(playerPosition.y < MAP_SIZE_Y-1)
+        if (keycode == Input.Keys.UP) {
+            if (playerPosition.y < MAP_SIZE_Y - 1)
                 playerPosition.y += 1;
         }
-        if(keycode == Input.Keys.DOWN) {
-
-            if(playerPosition.y > 0)
+        if (keycode == Input.Keys.DOWN) {
+            if (playerPosition.y > 0)
                 playerPosition.y -= 1;
         }
-
         return false;
     }
 }
